ef1f7e6f26b534a7560de0a3affea46cc146767c
Lantip <lantip@Lantips-MacBook-Pro.local>
2022-07-25 13:22:50 +0700

    update requirements

--- monitor/views.py
+++ monitor/views.py
@@ -6,10 +6,10 @@ from django.db.models import Q
 from itertools import chain
 
 def handler404(request, exception):    
-    return render(request, "404.html", status=404)
+    return render(request, "error_404.html", status=404)
 
 def handler500(request):
-    return render(request, "500.html", status=500)  
+    return render(request, "error_500.html", status=500)  
 
 def index(request):
     asing = PSEAsing.objects.all()
